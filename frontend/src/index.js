a4715c3e5b69aac409a0e7dd84795173de92e931
straightface <bethany.davies1998@gmail.com>
2025-10-07 08:12:11 +0100

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-createRoot(document.getElementById('root')).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
