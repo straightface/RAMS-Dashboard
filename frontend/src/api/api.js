a54fd73e31f87d324721e8c9639af6f18032d75b
straightface <bethany.davies1998@gmail.com>
2025-10-07 07:58:22 +0100

    api.js

---
+++ frontend/src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL || "https://rams-api.onrender.com";
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const loginUser = async (username, password) => {
+  const res = await api.post("/login", { username, password });
+  return res.data;
+};
+
+export const getHealth = async () => {
+  const res = await api.get("/api/health");
+  return res.data;
+};
+
+export const getProjects = async (token) => {
+  const res = await api.get("/api/projects/active", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
